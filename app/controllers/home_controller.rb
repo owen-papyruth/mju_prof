b7b74b92bf93566bcdfce368aa9622b3da8e246d
Owen <Owen@Owen-ui-MacBook-Pro.local>
2016-06-03 21:05:25 +0900

    search 결과 보여주는 페이지 재활용

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,17 @@ class HomeController < ApplicationController
   def index
     @prof_list = Prof.order(like: :desc).all
   end
-  
+
   def like
     Prof.where('id like :key', key:params[:prof_id]).update_all("like = like + 1")
     redirect_to :back
   end
-  
+
   def search_data
     data = params[:keyword]
     prof =  Prof.where('name like :keyword OR dept like :keyword', keyword:"%" + data +"%")
     @prof_list = prof.order(like: :desc).all
-    puts @prof_list.size
+    render 'index'
+    # puts @prof_list.size
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,44 @@ require 'nokogiri'
 require 'open-uri'
 
     # 크롤링 할 페이지의 URL
-    url = "http://www.mju.ac.kr/user/introduction_mju/professor/professor_list.jsp?deptCd=12755&id=mjukr_020206110200"
-    data = Nokogiri::HTML(open url)
-    
-    data_list = data.css('ul.profsList')
-    
-    size = data_list.search('li').size
-    
-    i = 0
-    while i < size  do
+    # 학과별 URL
+    list = Array["12913&id=mjukr_020206070200",
+                "12900&id=mjukr_020206010200",
+                "12901&id=mjukr_020206030200",
+                "12250&id=mjukr_020206040200",
+                "12908&id=mjukr_020206020200",
+                "12361&id=mjukr_020206050200",
+                "12370&id=mjukr_020206060200",
+                "12450&id=mjukr_020206080200",
+                "12500&id=mjukr_020206090200",
+                "12701&id=mjukr_020206100200",
+                "12755&id=mjukr_020206110200"]
+
+    # 학과
+    major = Array["컴퓨터공학과",
+                  "전기공학과",
+                  "전자공학과",
+                  "정보통신학과",
+                  "화학공학과",
+                  "신소재공학과",
+                  "환경에너지공학과",
+                  "토목환경공학과",
+                  "교통공학과",
+                  "기계공학과",
+                  "산업경영공학과"]
+
+    j = 0
+    while j < list.size do
+
+        url = "http://www.mju.ac.kr/user/introduction_mju/professor/professor_list.jsp?deptCd=" + list[j]
+        data = Nokogiri::HTML(open url)
+
+        data_list = data.css('ul.profsList')
+
+        size = data_list.search('li').size
+
+      i = 0
+      while i < size  do
         source = data_list.css('span.pic img')[i]
         profname = source.attr('alt')
         profimgUrl = source.attr('src')
@@ -25,9 +54,8 @@ require 'open-uri'
         # imgUrl : 교수 이미지
         # like : 좋아요수 default 0
         # dept : 학과 (일일히 타이핑)
-        Prof.create(name: profname, imgUrl: profimgUrl, like: 0, dept: '산업경영공학과')
+        Prof.create(name: profname, imgUrl: profimgUrl, like: 0, dept: major[j])
         i+=1
-    end
-    
-    #prof.save
-    
+      end
+      j += 1
+   end
